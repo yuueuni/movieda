5c9eb3359fff6fd970b35ef77da2496e847547f4
JHyuk2 <whdgurdl77@gmail.com>
2020-06-13 21:50:08 +0900

    Add UserFavorField | accounts - models

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-from ..movies.models import Actor, Director, Movie
+from movies.models import Actor, Director, Movie
 
 # Create your models here.
 class User(AbstractUser):
     birthday = models.DateTimeField(null=True)
     gender = models.BooleanField(default=True)
-    favorite_actors = models.ManyToManyField(Actor, null=True, blank=True, related_name='user_favor')
-    favorite_directors = models.ManyToManyField(Director, null=True, blank=True, related_name='user_favor')
-    favorite_movies = models.ManyToManyField(Movie, null=True, blank=True, related_name='user_favor')
+    favorite_actors = models.ManyToManyField(Actor, blank=True, related_name='users')
+    favorite_directors = models.ManyToManyField(Director, blank=True, related_name='users')
+    favorite_movies = models.ManyToManyField(Movie, blank=True, related_name='users')
+
+
+# 생각해보니까 없어도 될거같음.
+
+# from movies.models import Genre
+# class Favor(models.Model):
+#     favor_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favors')
+#     genre_name = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='favors')
+#     cnt = models.IntegerField()
--- movies/models.py
+++ movies/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.conf import settings
+# from django.contrib.auth import get_user_model
+User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
