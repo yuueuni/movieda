5372f9c896a0372b962eb4b9ba76f48d6270dfac
JHyuk2 <whdgurdl77@gmail.com>
2020-06-13 21:07:34 +0900

    Update accounts app - models & serializers | Add user_favors

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-
+from ..movies.models import Actor, Director, Movie
 
 # Create your models here.
 class User(AbstractUser):
     birthday = models.DateTimeField(null=True)
     gender = models.BooleanField(default=True)
+    favorite_actors = models.ManyToManyField(Actor, null=True, blank=True, related_name='user_favor')
+    favorite_directors = models.ManyToManyField(Director, null=True, blank=True, related_name='user_favor')
+    favorite_movies = models.ManyToManyField(Movie, null=True, blank=True, related_name='user_favor')
