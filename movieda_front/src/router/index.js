f21d7595857af5cd503944697e5988ad80547c41
JHyuk2 <whdgurdl77@gmail.com>
2020-06-15 12:50:56 +0900

    Install modules & Add accounts Vue

--- movieda_front/src/router/index.js
+++ movieda_front/src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+
+import LoginView from '../views/accounts/LoginView.vue'
+import SignupView from '../views/accounts/SignupView.vue'
+
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -11,13 +16,15 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/accounts/login',
+    name: 'Login',
+    component: LoginView,
+  },
+  {
+    path: '/accounts/signup',
+    name: 'Signup',
+    component: SignupView,
+  },
 ]
 
 const router = new VueRouter({
---
+++ movieda_front/src/views/accounts/SignupView.vue
@@ -0,0 +1,54 @@
+<template>
+	<div>
+		<h1>Signup</h1>
+		<div>
+			<label for="username">username: </label>
+			<input v-model="loginData.username" id="username" type="text">
+		</div>
+		<div>
+			<label for="password1">password: </label>
+			<input v-model="loginData.password" id="password1" type="password">
+		</div>
+		<div>
+			<label for="password2">password: </label>
+			<input v-model="loginData.password2" id="password2" type="password">
+		</div>
+		<div>
+			<label for="birthday">birthday: </label>
+			<input v-model="loginData.birthday" id="birthday" type="date">
+		</div>
+		<div>
+			<label for="gender">gender: </label>
+			<input v-model="loginData.gender" id="gender" type="number">
+		</div>
+		<div>
+			<button @click="signup">signup</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LoginView',
+		data() {
+			return {
+					loginData: {
+							username: null,
+							password1: null,
+							password2: null,
+							birthday: null,
+							gender: null,
+					}
+				}
+			},
+			methods: {
+				login() {
+					this.$emit('submit-signup-data', this.signupData)
+				}
+		}
+	}
+</script>
+
+<style>
+
+</style>
