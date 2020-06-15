a320c22d894cfc69adc3617cb1173aaed5160c2b
yoon <yuueuni@gmail.com>
2020-06-15 17:58:52 +0900

    Add user Profile | accounts - views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
+from movies.models import Review
+from movies.serializers import ReviewSerializer
+
+
+User = get_user_model()
+
+
+@api_view(['GET'])
+def profile(request, username):
+    user = User.objects.get(username=username)
+    serializer = UserSerializer(user)
+    review = Review.objects.filter(reviewer__pk=user.pk)
+    serializer_reviews = ReviewSerializer(review, many=True)
+    context = {
+        'profile': serializer.data,
+        'reviews': serializer_reviews.data,
+    }
+    return Response(context)
