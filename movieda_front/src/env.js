af9930fd57295df63a8efb7fba79468f37f6a795
Ubuntu <ubuntu@ip-172-31-47-96.ap-northeast-2.compute.internal>
2020-06-18 03:04:43 +0000

    add

--- movieda_front/src/env.js
+++ movieda_front/src/env.js
@@ -1,3 +1,3 @@
-const SERVER_URL='http://ec2-52-79-219-108.ap-northeast-2.compute.amazonaws.com:8000'
+const SERVER_URL='52.78.19.132:8000'
 // const SERVER_URL='http://localhost:8000'
 export default SERVER_URL
